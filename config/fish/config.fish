66afe662d270a80b404d5a226bfa1f6c2558d6e0
Kevin Wang <kwangsan@gmail.com>
2020-05-19 17:58:38 -0400

    add fish & starship

---
+++ config/fish/config.fish
@@ -0,0 +1,12 @@
+# ~/.config/fish/config.fish
+starship init fish | source
+
+# aliases
+alias g='git'
+alias gc='git checkout'
+alias gcm='git checkout master'
+alias grhh='git reset --hard HEAD'
+alias gp='git pull'
+alias k='kubectl'
+
+
