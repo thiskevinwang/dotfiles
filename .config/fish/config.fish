c1e820610b31233505eea61f0ed82a3fbc7a376b
Kevin Wang <kwangsan@gmail.com>
2020-06-15 09:10:06 -0400

    add aws-cli fish completion

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,5 +8,8 @@ alias gcm='git checkout master'
 alias grhh='git reset --hard HEAD'
 alias gp='git pull'
 alias k='kubectl'
+alias emacs='emacs -nw'
 
-
+# Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
+# https://github.com/aws/aws-cli/issues/1079#issuecomment-541997810
+complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
